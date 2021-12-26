11f37c330887dc63c00f42de85edfc9e512551c9
AbdulHamid-codes <abdulhamidlaspure@gmail.com>
2021-12-26 23:56:13 +0500

    Added visitor awards view

--- src/application/VisitorAwardController.java
+++ src/application/VisitorAwardController.java
@@ -1,5 +1,10 @@
 package application;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import db.dbConnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -7,8 +12,6 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 public class VisitorAwardController {
-    @FXML
-    private TextField year;
 
     @FXML
     private ListView<String> awards;
@@ -26,7 +29,28 @@ public class VisitorAwardController {
     }
 
     @FXML
-    void displayAwards(ActionEvent event) {
+    void displayAwards(ActionEvent event) throws Exception {
+      	try {
+    			Connection con=dbConnection.getConnection();
+    			Statement stmt  = con.createStatement();
+    			String sql = "SELECT * " +
+    		             "FROM pcbawards ";
 
+    		ResultSet rs    = stmt.executeQuery(sql);
+    		awards.getItems().add("name       Year      Winner ");
+    		while (rs.next()) {
+    			String line= rs.getString("name");
+    			line+="          "+Integer.toString(rs.getInt("year"));
+    			line+="          "+rs.getString("winner");
+    			awards.getItems().add(line);	
+    			}
+    		}
+    		catch(Exception e) {
+    			System.out.println(e);
+    			String s=e.getMessage();
+    			//messageInCreateSq.setText(s);
+    		}
     }
+
+
 }
